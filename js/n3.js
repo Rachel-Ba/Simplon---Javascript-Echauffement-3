1ee1094b3bf6d8ac0111e7a301380dc92565a0a2
RachelBa <42960560+Rachel-Ba@users.noreply.github.com>
2018-10-29 16:35:03 +0100

    Ajout de la partie 3 bonus

--- js/n3.js
+++ js/n3.js
@@ -1,8 +1,28 @@
+var user = {
+    login: "",
+    password: "",
+};
+
+var user1 = Object.create(user);
+user1.login = "lea@gmail.com";
+user1.password = "12345";
+
+var user2 = Object.create(user);
+user2.login = "emma@gmail.com";
+user2.password = "02468";
+
+var user3 = Object.create(user);
+user3.login = "louise@gmail.com";
+user3.password = "13579";
+
+var user4 = Object.create(user);
+user4.login = "jade@gmail.com";
+user4.password = "98765";
+
+//----------------------------------------------------------------------------------------------------------
 var ident = prompt("Veuillez entrer votre identifiant :");
-var login = "lea@gmail.com";
 
-
-while (ident!==login)
+while ((ident!==user1.login)&&(ident!==user2.login)&&(ident!==user3.login)&&(ident!==user4.login))
 {
 	if(ident.length<4)
 	{
@@ -21,14 +41,48 @@ while (ident!==login)
 	}
 }
 
+//----------------------------------------------------------------------------------------------------------
+
 var motDePasse = prompt("Veuillez entrer votre mot de passe :");
-var password = "12345";
 
-
-while (motDePasse!==password)
+if (ident===user1.login)
 {
-	alert("Ce n'est pas le bon mot de passe, recommencer");
-	motDePasse = prompt("Veuillez entrer votre mot de passe :");
+	while (motDePasse!==user1.password)
+	{
+		alert("Ce n'est pas le bon mot de passe, recommencer");
+		motDePasse = prompt("Veuillez entrer votre mot de passe :");
+	}
+	
+}
+
+if (ident===user2.login)
+{
+	while (motDePasse!==user2.password)
+	{
+		alert("Ce n'est pas le bon mot de passe, recommencer");
+		motDePasse = prompt("Veuillez entrer votre mot de passe :");
+	}
+	
+}
+
+if (ident===user3.login)
+{
+	while (motDePasse!==user3.password)
+	{
+		alert("Ce n'est pas le bon mot de passe, recommencer");
+		motDePasse = prompt("Veuillez entrer votre mot de passe :");
+	}
+	
+}
+
+if (ident===user4.login)
+{
+	while (motDePasse!==user4.password)
+	{
+		alert("Ce n'est pas le bon mot de passe, recommencer");
+		motDePasse = prompt("Veuillez entrer votre mot de passe :");
+	}
+	
 }
 
 alert("Bienvenue Lea, votre identifiant est "+ ident + " et votre mot de passe est " + motDePasse);
